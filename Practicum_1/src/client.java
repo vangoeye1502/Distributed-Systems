0f816deb1da75475987bd3bc7f8c03cdfc6ac700
jannesvangoeye <jannes.van.goeye@gmail.com>
2017-10-09 11:39:42 +0200

    client v0.1

--- Practicum_1/src/client.java
+++ Practicum_1/src/client.java
@@ -3,3 +3,37 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.*;
+
+public class client {
+    public static void main(String[] args) {
+        String IPAddress;
+        Socket sock = new Socket(IPAddress, 7896);
+
+        //Send file
+        File myFile = new File("E7060v1.2.zip");
+        byte[] mybytearray = new byte[(int) myFile.length()];
+
+        FileInputStream fis = new FileInputStream(myFile);
+        BufferedInputStream bis = new BufferedInputStream(fis);
+        //bis.read(mybytearray, 0, mybytearray.length);
+
+        DataInputStream dis = new DataInputStream(bis);
+        dis.readFully(mybytearray, 0, mybytearray.length);
+
+        OutputStream os = sock.getOutputStream();
+
+        //Sending file name and file size to the server
+        DataOutputStream dos = new DataOutputStream(os);
+        dos.writeUTF(myFile.getName());
+        dos.writeLong(mybytearray.length);
+        dos.write(mybytearray, 0, mybytearray.length);
+        dos.flush();
+
+        //Sending file data to the server
+        os.write(mybytearray, 0, mybytearray.length);
+        os.flush();
+
+        //Closing socket
+        sock.close();
+    }
+}
